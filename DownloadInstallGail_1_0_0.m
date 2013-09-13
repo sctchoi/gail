1a0c8ea735aba7f8e8fbc764369a8c7ba524b06b
fjhickernell <hickernell@iit.edu>
2013-09-12 22:58:24 -0500

    revised

--- DownloadInstallGail_1_0_0.m
+++ DownloadInstallGail_1_0_0.m
@@ -1,5 +1,4 @@
-%This mfile may be used to 
-%   download and install GAIL into 
+%This mfile may be used to download and install GAIL into 
 %   the location you choose
 %
 %   Step 1.  Place this .m file in where you want GAIL to go
@@ -7,15 +6,15 @@
 %   Step 2.  Run this mfile
 
 %% Download the package and change the directory
-unzip('http://math.iit.edu/~openscholar/sites/default/files/meshfree/files/gail_1.0.0.zip')
-cd('GAIL_1.0.0')
-cd('GAIL_Matlab')
+unzip(['http://math.iit.edu/~openscholar/sites/default/files/meshfree/'...
+    'files/gail_1.0.0.zip']) %download and unzip
+cd('GAIL_1.0.0') %get to the right subdirectory
+cd('GAIL_Matlab') %get to the right subdirectory
 
 %% Install Gail
-GAIL_Install 
+GAIL_Install %this installs GAIL
 
 %% Run a quick test
-muhat=meanMC_g(@(n) rand(n,1));
+muhat=meanMC_g(@(n) rand(n,1)); %run meanMC_g once
 disp(['The number, ' num2str(muhat,2) ', should be close to 0.5'])
-
 %   Then you should be ready to use GAIL
